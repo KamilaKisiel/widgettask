a7cf64c08618309ef2d77de1e341bc561978f370
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-09 21:28:35 +0200

    created main card structure

--- widget/src/components/MainCard.js
+++ widget/src/components/MainCard.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class MainCard extends Component {
     render () {
         return (
-            <div></div>
+            <div className='card'>
+                <img alt='icona pop' src={''}/>
+                <div className='songTitle'>
+                    <h1>Icona Pop</h1>
+                    <p>Still Don't Know</p>
+                </div>
+                <div className='buttonsPanel'>
+
+                </div>
+            </div>
         );
     }
 }
