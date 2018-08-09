5e35744817c36e7f494f7feba00e9caa71634fd5
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-09 21:43:01 +0200

    temporary image added

--- widget/src/components/MainCard.js
+++ widget/src/components/MainCard.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import image from '../../public/images/Icona_Pop'
 
 class MainCard extends Component {
     render () {
         return (
             <div className='card'>
-                <img alt='icona pop' src={''}/>
+                <img alt='icona pop' src={image}/>
                 <div className='songTitle'>
                     <h1>Icona Pop</h1>
                     <p>Still Don't Know</p>
