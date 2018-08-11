170734c53c745e1241d1a8fd95c7c70e8b8c6021
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-11 21:22:56 +0200

    image source added

--- widget/src/components/MainCard.js
+++ widget/src/components/MainCard.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import image from '../../public/images/Icona_Pop'
+// import image from '../images/from'
 
 class MainCard extends Component {
     render () {
         return (
             <div className='card'>
-                <img alt='icona pop' src={image}/>
+                <img alt='icona pop' src="https://lastfm-img2.akamaized.net/i/u/300x300/7eab07ac89694304890fcd6d51c72255.jpg"/>
                 <div className='songTitle'>
                     <h1>Icona Pop</h1>
                     <p>Still Don't Know</p>
