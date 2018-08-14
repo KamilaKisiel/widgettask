e1acfe300906b93e5740d15acd268840b8f513bc
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-14 22:22:07 +0200

    created a library for font awesome icons

--- widget/src/App.js
+++ widget/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import MainCard from './components/MainCard';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faShareAlt } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
+
+library.add(faShareAlt);
 
 class App extends Component {
   render() {
