7e75f4a61dfe901ba7be124a2feccc3de23aa01a
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-14 23:56:50 +0200

    additional div created to fix title position

--- widget/src/components/MainCard.js
+++ widget/src/components/MainCard.js
@@ -3,30 +3,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import image from '../images/icona-pop.jpg'
 
 class MainCard extends Component {
-    render () {
+    render() {
         return (
             <div className='card'>
-                <img className='icona' alt='icona pop' src={image}/>
-                <div className='songTitle'>
-                    <h1>Icona Pop</h1>
-                    <p>Still Don't Know</p>
+                <div className='imgSection'>
+                    <img className='icona' alt='icona pop' src={image} />
+                    <div className='songTitle'>
+                        <p>Icona Pop</p>
+                        <p>Still Don't Know</p>
+                    </div>
                 </div>
                 <div className='buttonsPanel'>
-                <button className='btn share'>
-                <FontAwesomeIcon icon="share-alt"></FontAwesomeIcon>
-                </button>
-                <button className='btn stepBackward'>
-                <FontAwesomeIcon icon="step-backward"></FontAwesomeIcon>
-                </button>
-                <button className='btn playButton'>
-                <FontAwesomeIcon icon="play"></FontAwesomeIcon>
-                </button>
-                <button className='btn stepForward'>
-                <FontAwesomeIcon icon="step-forward"></FontAwesomeIcon>
-                </button>
-                <button className='btn heart'>
-                <FontAwesomeIcon icon="heart"></FontAwesomeIcon>
-                </button>
+                    <button className='btn share'>
+                        <FontAwesomeIcon icon="share-alt"></FontAwesomeIcon>
+                    </button>
+                    <button className='btn stepBackward'>
+                        <FontAwesomeIcon icon="step-backward"></FontAwesomeIcon>
+                    </button>
+                    <button className='btn playButton'>
+                        <FontAwesomeIcon icon="play"></FontAwesomeIcon>
+                    </button>
+                    <button className='btn stepForward'>
+                        <FontAwesomeIcon icon="step-forward"></FontAwesomeIcon>
+                    </button>
+                    <button className='btn heart'>
+                        <FontAwesomeIcon icon="heart"></FontAwesomeIcon>
+                    </button>
 
                 </div>
             </div>
