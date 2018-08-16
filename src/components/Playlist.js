8232bc2113f844eb538631103710825e3b1fd840
KamilaKisiel <kisiel.kamila@gmail.com>
2018-08-16 22:49:32 +0200

    playlist styling: icons, scroll

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -17,38 +17,87 @@ const Playlist = ({ onRouteChange }) => {
                 <ul className='list'>
                     <li>
                         <div className='listItem'>
-                            <p className='songDuration'>3:16 | Icona Pop</p>
-                            <p className='songTitle'>Still Don't Know</p>
+                            <div>
+                                <p className='songDuration'>3:16 | Icona Pop</p>
+                                <p className='songTitle'>Still Don't Know</p>
+                            </div>
+                            <div className='songAction'>
+                                <button className='btn share smallBtn'>
+                                    <FontAwesomeIcon icon="share-alt"></FontAwesomeIcon>
+                                </button>
+                                <button className='btn heart smallBtn'>
+                                    <FontAwesomeIcon icon="heart"></FontAwesomeIcon>
+                                </button>
+                            </div>
                         </div>
                     </li>
                     <li>
                         <div className='listItem'>
-                            <p className='songDuration'>2:35 | Icona Pop</p>
-                            <p className='songTitle'>I Love it</p>
+                            <div>
+                                <p className='songDuration'>2:35 | Icona Pop</p>
+                                <p className='songTitle'>I Love it</p>
+                            </div>
+                            <div className='songAction'>
+                                <button className='btn share smallBtn'>
+                                    <FontAwesomeIcon icon="share-alt"></FontAwesomeIcon>
+                                </button>
+                                <button className='btn heart smallBtn'>
+                                    <FontAwesomeIcon icon="heart"></FontAwesomeIcon>
+                                </button>
+                            </div>
                         </div>
                     </li>
                     <li>
                         <div className='listItem'>
-                            <p className='songDuration'>2:50 | Icona Pop</p>
-                            <p className='songTitle'>Girlfriend</p>
+                            <div>
+                                <p className='songDuration'>2:50 | Icona Pop</p>
+                                <p className='songTitle'>Girlfriend</p>
+                            </div>
+                            <div className='songAction'>
+                                <button className='btn share smallBtn'>
+                                    <FontAwesomeIcon icon="share-alt"></FontAwesomeIcon>
+                                </button>
+                                <button className='btn heart smallBtn'>
+                                    <FontAwesomeIcon icon="heart"></FontAwesomeIcon>
+                                </button>
+                            </div>
                         </div>
                     </li>
                     <li>
                         <div className='listItem'>
-                            <p className='songDuration'>3:07 | Icona Pop</p>
-                            <p className='songTitle'>We Got the World</p>
+                            <div>
+                                <p className='songDuration'>3:07 | Icona Pop</p>
+                                <p className='songTitle'>We Got the World</p>
+                            </div>
+                            <div className='songAction'>
+                                <button className='btn share smallBtn'>
+                                    <FontAwesomeIcon icon="share-alt"></FontAwesomeIcon>
+                                </button>
+                                <button className='btn heart smallBtn'>
+                                    <FontAwesomeIcon icon="heart"></FontAwesomeIcon>
+                                </button>
+                            </div>
                         </div>
                     </li>
                     <li>
                         <div className='listItem'>
-                            <p className='songDuration'>3:24 | Icona Pop</p>
-                            <p className='songTitle'>Nights Like This</p>
+                            <div>
+                                <p className='songDuration'>3:24 | Icona Pop</p>
+                                <p className='songTitle'>Nights Like This</p>
+                            </div>
+                            <div className='songAction'>
+                                <button className='btn share smallBtn'>
+                                    <FontAwesomeIcon icon="share-alt"></FontAwesomeIcon>
+                                </button>
+                                <button className='btn heart smallBtn'>
+                                    <FontAwesomeIcon icon="heart"></FontAwesomeIcon>
+                                </button>
+                            </div>
                         </div>
                     </li>
                 </ul>
             </div>
-            <div className='clearFloat'></div>
-
+            <div className='scrollBar'>.</div>
         </div>
     );
 }
